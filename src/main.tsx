1c5f0ef3bcfa8ac9a765efdcae0a2d6bf03199dc
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-10-05 00:44:56 -0300

    feat: init application

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'
+import App from './components/app'
+import React from 'react'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
