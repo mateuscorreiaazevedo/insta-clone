e1bf066cc5f120e9f5fec5f0fc180b754216cb28
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-10-05 13:19:56 -0300

    chore: initial development of auth context and creation of tokenUtil

---
+++ src/contexts/auth-context.tsx
@@ -0,0 +1,62 @@
+import { ChildrenType } from '../types/children'
+import React from 'react'
+import { tokenUtil } from '../utils/token'
+
+type AuthProps = {
+  user: any | null
+  auth: boolean
+  loading: boolean
+  error: string
+  handleLogin: () => void
+  handleRegister: () => void
+  handleLogOut: () => void
+
+}
+
+const Context = React.createContext<AuthProps | null>(null)
+
+export const Provider = ({ children }: ChildrenType) => {
+  const [user, setUser] = React.useState<any | null>(null)
+  const [auth, setAuth] = React.useState(false)
+  const [error, setError] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
+  const token = tokenUtil.get()
+
+  const getUser = async () => {
+    try {
+      setLoading(true)
+      setUser('')
+    } catch (error) {
+      setError((error as any).message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  React.useEffect(() => {
+    if (token) {
+      setAuth(true)
+      getUser()
+    }
+  }, [])
+
+  const handleLogin = async () => {
+
+  }
+
+  const handleRegister = async () => {
+
+  }
+
+  const handleLogOut = () => {
+    tokenUtil.clear()
+    setUser(null)
+    window.location.reload()
+  }
+
+  return (
+    <Context.Provider value={{ user, auth, loading, error, handleLogin, handleLogOut, handleRegister }}>
+      {children}
+    </Context.Provider>
+  )
+}
