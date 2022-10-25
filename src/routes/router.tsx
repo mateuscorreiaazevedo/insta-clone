a0c9b2532f24e92f9ff3fc5fa5bdc878821da420
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-10-25 19:28:07 -0300

    fix: adjusting history router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,26 +1,35 @@
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 import { useAuth } from '../contexts/auth-context'
-import { BrowserRouter } from 'react-router-dom'
+import { Loader } from '../components/ui/loader'
+import { createBrowserHistory } from 'history'
 import { PrivateRoutes } from './private'
 import { PublicRoutes } from './public'
 import React from 'react'
-import { Loader } from '../components/ui/loader'
 
 export const AppRouter = () => {
   const { auth, loader } = useAuth()
 
   if (loader) {
     return (
-    <div style={{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
       <Loader/>
     </div>
     )
   }
 
   return (
-    <BrowserRouter>
+    <HistoryRouter history={createBrowserHistory()}>
       <React.Suspense fallback={<p>Carregando...</p>}>
         {auth ? <PrivateRoutes/> : <PublicRoutes/>}
       </React.Suspense>
-    </BrowserRouter>
+    </HistoryRouter>
   )
 }
